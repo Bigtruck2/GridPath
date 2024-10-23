609af17b9f8f44bfeea943e4d3f9a19a8dee29c4
Bigtruck2 <bigburrito363@gmail.com>
2024-10-23 11:50:46 -0400

    second

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[][] grid  = {{12, 3, 4, 13,5},{11,21,2,14,16},
+                {7,8,9,15,0},{10,17,20,19,1},{18,22,30,25,6}};
+        GridPath gridPath = new GridPath(grid);
+        Location location = gridPath.getNextLoc(4,3);
+        System.out.println(gridPath.sumPath(1,1));
     }
 }
